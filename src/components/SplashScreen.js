78cca0d0d4ec5e5b90ead55c8244d564a67e3e5f
cchanEG <connorjosephchan@gmail.com>
2019-02-10 16:27:10 -0500

    Remove unused code

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -8,35 +8,6 @@ const SplashSection = styled.section`
     padding: 45px 0 15px;
 `;
 
-const SplashDie = styled.div`
-    background: #FFF;
-    height: 150px;
-    width: 150px;
-    border: 1px solid #000;
-    border-radius: 10px;
-    position: relative;
-    margin: 0 auto 10px auto;
-    padding-bottom: 15px;
-    transform: rotate(33deg);
-`;
-
-const SplashDotRow = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin: 17px;
-
-    :nth-child(2) {
-        visibility: hidden;
-    }
-`;
-
-const SplashDieDot = styled.div`
-    width: 25px;
-    height: 25px;
-    border-radius: 50%;
-    background: #000;
-`;
-
 const SplashScreen = () => {
     return (
         <React.Fragment>
